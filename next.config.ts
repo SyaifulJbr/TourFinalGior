84842a7fc9ef0fba43107aafc8f1066fc1c1d5ac
Giorgino Malik <giorginomalik@gmail.com>
2025-10-22 00:10:19 +0000

    fix: update basePath to new repository name

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/GiorBaliTour',
+  basePath: '/TourFinalGior',
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,
